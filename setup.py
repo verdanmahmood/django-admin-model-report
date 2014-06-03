5b8ff268baf604eaef1d9014dd07a1ccc2276b9a
Verdan <verdan.mahmood@gmail.com>
2014-06-04 03:54:45 +0500

    - updating htmls

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from os.path import join, dirname
 from setuptools import setup, find_packages
 
 
-version = __import__('model_report').__version__
+version = __import__('admin_model_report').__version__
 
 
 LONG_DESCRIPTION = """
-django-model-report
+django-admin-model-report
 ===================
 
-django reports integrated with highcharts
+django admin reports integrated with highcharts
 
-    $ git clone git://github.com/juanpex/django-model-report.git
+    $ git clone git@github.com:verdanmahmood/django-admin-model-report.git
 """
 
 
@@ -23,20 +23,20 @@ def long_description():
         return LONG_DESCRIPTION
 
 
-setup(name='django-model-report',
+setup(name='django-admin-model-report',
       version=version,
       author='juanpex',
       author_email='jpma55@gmail.com',
       description='Django reports integrated with highcharts.',
       license='BSD',
-      keywords='django, model, report, reports, highcharts, chart, charts',
+      keywords='django, model, django admin, report, reports, highcharts, chart, charts',
       url='https://github.com/juanpex/django-model-report',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       long_description=long_description(),
       install_requires=[
-        'django>=1.3',
+        'django>=1.5.5',
         'reportlab',
         'html5lib',
         'BeautifulSoup',
